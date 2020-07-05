b0a2d460753e3579889bbcefd29e5c12cffd849c
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-05 14:24:48 +0700

    database migration

--- database/migrations/2020_07_05_071711_create_followers_table.php
+++ database/migrations/2020_07_05_071711_create_followers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateFollowersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('followers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_akun');
+            $table->integer('id_akunfollowers');
+            // $table->timestamp();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('followers');
     }
 }
---
+++ database/migrations/2020_07_05_071815_create_artikel_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->bigIncrements('id_artikel');
+            $table->integer('id_akun');
+            $table->string('judul');
+            $table->text('isi');
+            $table->varchar('slug');
+            $table->varchat('tag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
