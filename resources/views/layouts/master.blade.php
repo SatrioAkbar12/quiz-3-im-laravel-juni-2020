447f810a51d97fd6a3b570f34aac1505af18fc7d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-05 16:04:45 +0700

    template master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -332,7 +332,8 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+          {{-- <h1 class="h3 mb-4 text-gray-800">Blank Page</h1> --}}
+          @yield('konten')
 
         </div>
         <!-- /.container-fluid -->
