51516226e0ab4c25e722074b100625e6bd5a26ec
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-05 15:31:52 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,14 +13,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
+// Route::post('/items', 'ItemController@store'); // menyimpan data
+// Route::get('/items', 'ItemController@index'); // menampilkan semua
+// Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id
+// Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
+// Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
+// Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+
+Route::get('/', function() {
+    return view('erd');
 });
 
-Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
-Route::post('/items', 'ItemController@store'); // menyimpan data
-Route::get('/items', 'ItemController@index'); // menampilkan semua
-Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
-Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
-Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+Route::get('/artikel', 'ArtikelController@index');
