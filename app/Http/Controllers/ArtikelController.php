768c250f6b28700ae4f9116c5bda76575eacf8b4
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-05 15:44:56 +0700

    controller untuk artikel

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ArtikelModel;
 
 class ArtikelController extends Controller
 {
-    //
+    public function index(){
+        return view('artikel/index');
+    }
+
+    public function create(){
+        return view('artikel/form_artikel');
+    }
+
+    public function store(Request $request){
+        $store = ArtikelModel::insert_artikel($request);
+
+        return redirect('/artikel');
+    }
+
+    public function detail($id){
+        $artikel = ArtikelModel::get_with_idArtikel($id);
+
+        return view('artikel/detail_artikel', ['artikel' => $artikel]);
+    }
+
+    public function edit($id){
+        $artikel = ArtikelModel::get_with_idArtikel($id);
+
+        return view('artikel/form_edit', ['artikel' => $artikel]);
+    }
+
+    public function update(Request $request){
+        $update = ArtikelModel::update_artikel($request);
+
+        return redirect(route('artikel_detail', ['id' => $request->id_artikel]));
+    }
+
+    public function delete($id){
+        $delete = ArtikelModel::delete_artikel($id);
+
+        return redirect('/artikel');
+    }
 }
