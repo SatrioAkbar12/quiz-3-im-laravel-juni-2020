3166a8c08731fdaaac62b499858a6dbaa13b3993
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-05 16:35:07 +0700

    view diagram erd

--- resources/views/erd.blade.php
+++ resources/views/erd.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts/master')
 
+@section('head_title', 'Diagram ERD Quiz 3')
+
 @section('konten')
     <h1>Diagram ERD Quiz 3</h1>
     <img src="{{ asset('/images/erd_quiz3.png') }}" class="img-fluid">
