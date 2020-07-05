b3d11abab11420f69645319c94ca5a6bd372cc0c
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2020-07-05 19:38:48 +0700

    view form buat artikel

--- resources/views/artikel/form_artikel.blade.php
+++ resources/views/artikel/form_artikel.blade.php
@@ -3,7 +3,7 @@
 @section('head_title', 'Buat Artikel')
 
 @section('konten')
-    <h1>Buat Artikel Barumu</h1>
+    <h1>Buat Artikel Baru</h1>
 
     <form action="/artikel" method="POST">
         @csrf
@@ -17,7 +17,7 @@
         </div>
         <div class="form-group">
             <label for="tag_artikel">Tag Artikel :</label>
-            <textarea name="tag" id="tag_artikel" class="form-control" placeholder="contoh tag 1,contoh tag 2,contoh tag 3,..." required></textarea>
+            <input type="text" name="tag" id="tag_artikel" class="form-control" placeholder="Contoh : tag 1,tag 2,tag 3,..." required>
         </div>
         <input type="submit" class="btn btn-success" value="Simpan">
         <a href="/artikel" class="btn btn-danger">Batal</a>
